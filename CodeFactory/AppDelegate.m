da381f33aa8854097215b45212fc550f4ebcd748
Abdurrahman AVCI <abdurrahmanavci@Abdurrahmans-Mac.local>
2013-07-05 19:33:45 +0300

    Initial Commit

---
+++ CodeFactory/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  CodeFactory
+//
+//  Created by Abdurrahman AVCI on 7/5/13.
+//  Copyright (c) 2013 Abdurrahman AVCI. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ CodeFactory/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CodeFactory
+//
+//  Created by Abdurrahman AVCI on 7/5/13.
+//  Copyright (c) 2013 Abdurrahman AVCI. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
